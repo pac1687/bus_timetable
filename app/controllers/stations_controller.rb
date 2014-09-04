cefea03c25ba2e704ccc7e6dcc3f032fe732e003
Carolyn Huynh & Justin Pacubas <student@epicodus.com>
2014-09-04 10:50:46 -0700

    Create error partial

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -4,7 +4,18 @@ class StationsController < ApplicationController
   end
 
   def new
-    @stations = Station.new
+    @station = Station.new
+  end
+
+  def create
+    @station = Station.new(params[:station])
+    if @station.save
+      flash[:notice] = "#{@station.name} was added to the Bus Timetable."
+      redirect_to("/stations/#{@station.id}")
+    else
+      @stations = Station.all
+      render('/stations/new')
+    end
   end
 
 end
