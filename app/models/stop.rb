08c50804cb47d94d87dbd9777ad745874e1174f8
Carolyn Huynh & Justin Pacubas <student@epicodus.com>
2014-09-04 09:21:43 -0700

    Fix spec bugs on numericality

--- app/models/stop.rb
+++ app/models/stop.rb
@@ -1,6 +1,6 @@
 class Stop < ActiveRecord::Base
-  validates :station_id, numericality => true
-  validates :line_id, numericality => true
+  validates :station_id, numericality: true
+  validates :line_id, numericality: true
   belongs_to :station
   belongs_to :line
 end
